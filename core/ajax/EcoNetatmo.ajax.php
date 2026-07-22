8bdcf017d0219ec91bd8473dda0304d61dc77433
bernard-dandrea <60914791+bernard-dandrea@users.noreply.github.com>
2026-07-22 18:43:01 +0200

    Add files via upload

--- core/ajax/EcoNetatmo.ajax.php
+++ core/ajax/EcoNetatmo.ajax.php
@@ -1,4 +1,8 @@
 <?php
+
+
+// Last Modified : 2026/07/22 14:10:19
+
 /* This file is part of Jeedom.
  *
  * Jeedom is free software: you can redistribute it and/or modify
@@ -23,10 +27,6 @@ try {
         throw new Exception(__('401 - Accès non autorisé', __FILE__));
     }
 
-    /* Fonction permettant l'envoi de l'entête 'Content-Type: application/json'
-    En V3 : indiquer l'argument 'true' pour contrôler le token d'accès Jeedom
-    En V4 : autoriser l'exécution d'une méthode 'action' en GET en indiquant le(s) nom(s) de(s) action(s) dans un tableau en argument
-    */
     ajax::init();
 
 
@@ -58,6 +58,12 @@ try {
         $EcoNetatmo  = $eqLogic->counters_import($consumption_type,$source_type);
         ajax::success($EcoNetatmo);
     }
+
+    if (init('action') == 'enable_cron') {
+        EcoNetatmo::enable_cron(init('enable'));
+        ajax::success();
+    }
+
 
     throw new Exception(__('Aucune méthode correspondante à', __FILE__) . ' : ' . init('action'));
     /*     * *********Catch exeption*************** */
